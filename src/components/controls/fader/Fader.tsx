9b641c2d21c892cca94cba0433df8f2c21e2a808
Jacob Rich <charles.jacob.rich@gmail.com>
2025-01-14 21:21:56 -0500

    add horizontal/vertical handling

--- src/components/controls/fader/Fader.tsx
+++ src/components/controls/fader/Fader.tsx
@@ -1,8 +1,8 @@
 import './Fader.scss';
 
-function Fader() {
+function Fader(props: { orientation?: string }) {
   return (
-    <div className='fader-root'>
+    <div className={`fader-root ${props.orientation ?? 'vertical'}`}>
       <div className='fader-track'>
         <div className='fader-knob-edge'>
           <div className='fader-knob-top'>
